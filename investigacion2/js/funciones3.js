2cae4a4b2e41833ecbd0ff1eb689a1cf05acf192
C4ntona <92079574+C4ntona@users.noreply.github.com>
2022-02-13 12:21:14 +0100

    Add files via upload

---
+++ investigacion2/js/funciones3.js
@@ -0,0 +1,8 @@
+function cambiarEstado()
+{
+	let nombre_clase=document.getElementById("parrafo_lorem").className;
+	if (nombre_clase=="verde")
+		{document.getElementById("parrafo_lorem").className="rojo";}
+	else
+		{document.getElementById("parrafo_lorem").className="verde";}
+}
